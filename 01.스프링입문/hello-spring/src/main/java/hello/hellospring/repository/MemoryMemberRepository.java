431e1cf86e240b1454d748aa074c7668bb8ba39f
ykmxxi <ykmxxi97@gmail.com>
2022-11-02 13:53:58 +0900

    3.회원관리 예제 : MemoryMemberRepository.java 추가

---
+++ 01.스프링입문/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,37 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 실무에서는 동시성 문제가 발생할 수 있어 공유되는 변수 선언에 주의해야 함
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // null 이 반환될 가능성이 있으면 Optional 로 감싸줌
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
