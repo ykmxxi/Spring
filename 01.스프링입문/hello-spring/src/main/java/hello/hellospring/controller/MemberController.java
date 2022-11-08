520f6a3ac46698d68449e575c9702005f5414945
ykmxxi <ykmxxi97@gmail.com>
2022-11-08 22:27:16 +0900

    5.회원 웹 기능 - 회원 등록

--- 01.스프링입문/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ 01.스프링입문/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,5 +16,24 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    /**
+     * @GetMapping 은 주로 데이터를 조회 할 때 사용
+     * @PostMapping 은 주로 데이터 등록 할 때 사용
+     */
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
