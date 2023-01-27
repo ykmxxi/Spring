80870d390cf340f4fe82ae423ad460ef6a092a16
ykmxxi <ykmxxi97@gmail.com>
2023-01-27 16:01:46 +0900

    feat: transaction 처리

--- dmaker/src/main/java/com/devmaker/dmaker/service/DmakerService.java
+++ dmaker/src/main/java/com/devmaker/dmaker/service/DmakerService.java
@@ -4,25 +4,43 @@ import com.devmaker.dmaker.entity.Developer;
 import com.devmaker.dmaker.repository.DeveloperRepository;
 import com.devmaker.dmaker.type.DeveloperLevel;
 import com.devmaker.dmaker.type.DeveloperSkillType;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DmakerService {
     private final DeveloperRepository developerRepository;
+    private final EntityManager em;
 
     @Transactional
     public void createDeveloper() {
-        Developer developer = Developer.builder()
-                .developerLevel(DeveloperLevel.JUNIOR)
-                .developerSkillType(DeveloperSkillType.BACK_END)
-                .experiencedYears(2)
-                .name("Kim")
-                .age(28)
-                .build();
+        EntityTransaction transaction = em.getTransaction();
 
-        developerRepository.save(developer); // 영속화, DB에 저장
+        try {
+            transaction.begin();
+
+            // business logic start
+            Developer developer = Developer.builder()
+                    .developerLevel(DeveloperLevel.JUNIOR)
+                    .developerSkillType(DeveloperSkillType.BACK_END)
+                    .experiencedYears(2)
+                    .name("Kim")
+                    .age(28)
+                    .build();
+
+            developerRepository.save(developer); // 영속화, DB에 저장
+            // business logic end
+
+            transaction.commit();
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        }
+
     }
 }
