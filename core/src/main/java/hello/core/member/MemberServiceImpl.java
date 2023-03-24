87f461daf85262d15c21593373f81bd931b2d25c
ykmxxi <ykmxxi97@gmail.com>
2023-03-24 11:31:56 +0900

    Add #1- comments
    현재 코드의 문제점 주석 작성

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+	// 현재 문제점: 추상화에도 의존하고, 구현체에도 의존하고 있는 코드 -> DIP 위반
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
 
 	@Override
