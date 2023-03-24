173e85cbdf895a2c5dca7bf7230304fa145d47f7
ykmxxi <ykmxxi97@gmail.com>
2023-03-24 17:36:16 +0900

    Implement #3- appconfig
    AppConfig 구현을 통해 관심사 분리
    추상화와 구현체에 모두 의존하던 코드를 개선

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-	// 현재 문제점: 추상화에도 의존하고, 구현체에도 의존하고 있는 코드 -> DIP 위반
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	private final MemberRepository memberRepository;
+
+	public MemberServiceImpl(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	@Override
 	public void join(Member member) {
--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,12 +2,21 @@ package hello.core.member;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import hello.core.AppConfig;
+
 class MemberServiceTest {
 
-	MemberService memberService = new MemberServiceImpl();
+	MemberService memberService;
+
+	@BeforeEach
+	void beforeEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberService();
+	}
 
 	@DisplayName("회원가입 기능")
 	@Test
