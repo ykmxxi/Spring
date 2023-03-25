076417cacebff7b5741aa51da6ed24e762fd7fdb
ykmxxi <ykmxxi97@gmail.com>
2023-03-25 15:08:26 +0900

    Apply #3- spring framework
    스프링 적용
    - `@Configuration`, `@Bean`등록
    - ApplicationContext를 통해 빈으로 등록된 것을 사용

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -10,9 +13,14 @@ public class OrderApp {
 
 	public static void main(String[] args) {
 
-		AppConfig appConfig = new AppConfig();
-		MemberService memberService = appConfig.memberService();
-		OrderService orderService = appConfig.orderService();
+//		 AppConfig appConfig = new AppConfig();
+//		 MemberService memberService = appConfig.memberService();
+//		 OrderService orderService = appConfig.orderService();
+
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+		OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
 		Long memberId = 1L;
 		memberService.join(new Member(memberId, "memberA", Grade.VIP));
