1a1ac69244710d320acf48c2c5bcf6add6019c74
ykmxxi <ykmxxi97@gmail.com>
2023-03-28 14:25:44 +0900

    Add #7- singleton container test
    스프링 컨테이너(기본: 싱글톤 방식) 테스트 추가
    - 스프링 컨테이너는 싱글톤 방식으로 객체를 관리

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
@@ -26,6 +27,20 @@ public class SingletonTest {
 		System.out.println("memberService2 = " + memberService2);
 
 		assertThat(memberService1).isNotSameAs(memberService2);
+	}
+
+	@Test
+	@DisplayName("스프링 컨테이너와 싱글톤")
+	void springContainer() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+		MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+		System.out.println("memberService1 = " + memberService1);
+		System.out.println("memberService2 = " + memberService2);
+
+		assertThat(memberService1).isSameAs(memberService2);
 	}
 
 }
