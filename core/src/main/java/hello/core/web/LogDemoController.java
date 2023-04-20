e75dea5d2596529ed4e776f60f30c9558ede8d57
ykmxxi <ykmxxi97@gmail.com>
2023-04-20 13:40:44 +0900

    Add #15- `ObjectProvider`
    - `ObjectProvider`로 request scope bean 오류 해결

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +15,13 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
+		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.setRequestURL(requestURL);
 
 		myLogger.log("controller test");
