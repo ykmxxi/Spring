162f5d1fc37cdf4c2ef8fc1d67e098c24f418636
ykmxxi <ykmxxi97@gmail.com>
2023-06-16 15:15:39 +0900

    Implement #17- member save in db and test
    - DB에 회원 객체 저장및 테스트

---
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,20 @@
+package hello.jdbc.repository;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import hello.jdbc.domain.Member;
+
+class MemberRepositoryV0Test {
+
+	MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+	@Test
+	void crud() throws SQLException {
+		Member member = new Member("memberV0", 10000);
+
+		repository.save(member);
+	}
+
+}
