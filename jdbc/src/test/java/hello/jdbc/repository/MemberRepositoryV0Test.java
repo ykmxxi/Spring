830471301fa5845fca54d85c8d1aac8fb7771205
ykmxxi <ykmxxi97@gmail.com>
2023-06-16 15:43:53 +0900

    Implement #17- member inquiry in db and test
    - member_id (PK)를 이용한 회원 조회

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,20 +1,29 @@
 package hello.jdbc.repository;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.sql.SQLException;
 
 import org.junit.jupiter.api.Test;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
 	MemberRepositoryV0 repository = new MemberRepositoryV0();
 
 	@Test
 	void crud() throws SQLException {
+		// save
 		Member member = new Member("memberV0", 10000);
-
 		repository.save(member);
+
+		// findById
+		Member findMember = repository.findById(member.getMemberId());
+		log.info("findMember={}", findMember);
+		assertThat(findMember).isEqualTo(member);
 	}
 
 }
