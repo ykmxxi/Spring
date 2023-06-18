3d6a28a1ad6c8327861d4e3f2918aaa1965289a0
ykmxxi <ykmxxi97@gmail.com>
2023-06-18 15:51:27 +0900

    Test #19- connection pool test
    - `HikariDataSource` 사용

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -11,6 +11,8 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -30,6 +32,20 @@ public class ConnectionTest {
 		// DriverManagerDataSource: 항상 새로운 커넥션을 획득
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 		useDataSource(dataSource);
+	}
+
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		// 커넥션 풀링
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(10);
+		dataSource.setPoolName("MyPool");
+
+		useDataSource(dataSource);
+		Thread.sleep(1000);
 	}
 
 	private void useDataSource(DataSource dataSource) throws SQLException {
