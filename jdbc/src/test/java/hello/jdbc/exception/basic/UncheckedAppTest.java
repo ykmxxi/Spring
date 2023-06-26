c27e7eef141f3c582c84dc796003b820c6807314
ykmxxi <ykmxxi97@gmail.com>
2023-06-26 11:10:11 +0900

    Implement #25- unchecked exception test in web application
    - 웹 애플리케이션에서 런타임 예외를 사용하면 서비스, 컨트롤러는 예외에 의존하지 않음

---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,83 @@
+package hello.jdbc.exception.basic;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class UncheckedAppTest {
+
+	@Test
+	void unchecked() {
+		Controller controller = new Controller();
+		assertThatThrownBy(controller::request)
+			.isInstanceOf(RuntimeException.class);
+	}
+
+	static class Controller {
+
+		Service service = new Service();
+
+		public void request() {
+			service.logic();
+		}
+
+	}
+
+	static class Service {
+
+		Repository repository = new Repository();
+		NetworkClient networkClient = new NetworkClient();
+
+		public void logic() {
+			repository.call();
+			networkClient.call();
+		}
+
+	}
+
+	static class NetworkClient {
+
+		public void call() {
+			throw new RuntimeConnectException("연결 실패");
+		}
+
+	}
+
+	static class Repository {
+
+		public void call() {
+			try {
+				runSQL();
+			} catch (SQLException e) {
+				throw new RuntimeSQLException(e);
+			}
+		}
+
+		public void runSQL() throws SQLException {
+			throw new SQLException("ex");
+		}
+
+	}
+
+	static class RuntimeConnectException extends RuntimeException {
+
+		public RuntimeConnectException(String message) {
+			super(message);
+		}
+
+	}
+
+	static class RuntimeSQLException extends RuntimeException {
+
+		public RuntimeSQLException(Throwable cause) {
+			super(cause);
+		}
+
+	}
+
+}
