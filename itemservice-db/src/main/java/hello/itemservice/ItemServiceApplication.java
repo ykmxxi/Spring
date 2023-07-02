edbc662b7a55988b0b5c3c4717db1a50903be983
ykmxxi <ykmxxi97@gmail.com>
2023-07-02 13:16:57 +0900

    Implement #31- apply SimpleJdbcInsert in item repository
    - SimpleJdbcInsert를 적용해 INSERT SQL(save) 간편화

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -6,12 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-import hello.itemservice.config.JdbcTemplateV2Config;
+import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
 
 // @Import(MemoryConfig.class)
 // @Import(JdbcTemplateV1Config.class)
-@Import(JdbcTemplateV2Config.class)
+// @Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
