eacf609593229a686fd33e484f97ca4d21c9caa9
ykmxxi <ykmxxi97@gmail.com>
2023-07-02 13:04:14 +0900

    Implement #31- configuration file

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-import hello.itemservice.config.JdbcTemplateV1Config;
+import hello.itemservice.config.JdbcTemplateV2Config;
 import hello.itemservice.repository.ItemRepository;
 
 // @Import(MemoryConfig.class)
-@Import(JdbcTemplateV1Config.class)
+// @Import(JdbcTemplateV1Config.class)
+@Import(JdbcTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
