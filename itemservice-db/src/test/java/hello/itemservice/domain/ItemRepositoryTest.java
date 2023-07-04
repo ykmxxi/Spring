8307f7183f4de77bc9a4c0ffdae239925791f9a0
ykmxxi <ykmxxi97@gmail.com>
2023-07-04 11:49:23 +0900

    Test #33- using transaction feature
    - 테스트 격리와 반복 실행을 위해 트랜잭션 기능 추가

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,9 +5,13 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
@@ -20,12 +24,25 @@ class ItemRepositoryTest {
 	@Autowired
 	ItemRepository itemRepository;
 
+	// 트랜잭션 관련 코드
+	@Autowired
+	PlatformTransactionManager transactionManager;
+	TransactionStatus status;
+
+	@BeforeEach
+	void beforeEach() {
+		// 트랜잭션 시작
+		status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+	}
+
 	@AfterEach
 	void afterEach() {
 		//MemoryItemRepository 의 경우 제한적으로 사용
 		if (itemRepository instanceof MemoryItemRepository) {
 			((MemoryItemRepository)itemRepository).clearStore();
 		}
+		// 트랜잭션 롤백
+		transactionManager.rollback(status);
 	}
 
 	@Test
