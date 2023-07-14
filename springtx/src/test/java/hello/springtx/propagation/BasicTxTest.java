ffbefb4336b08530ca43b532fdd8e02816e6aad6
ykmxxi <ykmxxi97@gmail.com>
2023-07-14 14:05:27 +0900

    Test #47- default transaction propagation
    - REQUIRED(default) 전파 옵션 사용
    - 내부, 외부 트랜잭션 모두 커밋

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -86,4 +86,22 @@ class BasicTxTest {
 		txManager.rollback(tx2);
 	}
 
+	@DisplayName("트랜잭션 전파: 내부, 외부 트랜잭션 커밋")
+	@Test
+	void inner_commit() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+		log.info("내부 트랜잭션 시작");
+		TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+		log.info("내부 트랜잭션 커밋");
+		txManager.commit(inner);
+
+		log.info("외부 트랜잭션 커밋");
+		txManager.commit(outer);
+	}
+
 }
