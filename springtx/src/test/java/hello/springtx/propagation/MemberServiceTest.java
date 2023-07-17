c079e5d6e3f19fca6255fcb1465238e740143591
ykmxxi <ykmxxi97@gmail.com>
2023-07-17 11:41:03 +0900

    Test #49- log repository runtime exception generation
    - 서비스 계층에 트랜잭션이 없을 때 예외 발생, 롤백

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,7 +1,9 @@
 package hello.springtx.propagation;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +26,7 @@ class MemberServiceTest {
 	 * memberRepository	@Transactional: ON
 	 * logRepository	@Transactional: ON
 	 */
+	@DisplayName("서비스 계층에 트랜잭션이 없을 때: 두 리포지토리 모두 커밋 상황")
 	@Test
 	void outerTxOff_success() {
 		// given
@@ -36,6 +39,26 @@ class MemberServiceTest {
 		// junit.jupiter 라이브러리 사용
 		assertTrue(memberRepository.find(username).isPresent());
 		assertTrue(logRepository.find(username).isPresent());
+	}
+
+	/**
+	 * memberService 	@Transactional: OFF
+	 * memberRepository	@Transactional: ON
+	 * logRepository	@Transactional: ON, RuntimeException
+	 */
+	@DisplayName("서비스 계층에 트랜잭션이 없을 때: logRepository 예외 발생, 롤백")
+	@Test
+	void outerTxOff_fail() {
+		// given
+		String username = "로그예외_outerTxOff_fail";
+
+		// when
+		assertThatThrownBy(() -> memberService.joinV1(username))
+			.isInstanceOf(RuntimeException.class);
+
+		// then: 완전히 롤백되지 않고 member 데이터가 DB에 저장(트랜잭션이 분리되어 있음) -> 데이터 정합성 문제 발생
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isEmpty());
 	}
 
 }
