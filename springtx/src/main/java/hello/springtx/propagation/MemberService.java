0f860f8cf892010f19f3f2c819cb156058e32740
ykmxxi <ykmxxi97@gmail.com>
2023-07-17 10:49:10 +0900

    Implement #49- member service
    - 회원 저장, DB 로그 저장
    - 회원 저장, DB 로그 저장 예외 발생 시 정상 흐름 처리

---
+++ springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -0,0 +1,49 @@
+package hello.springtx.propagation;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+	private final LogRepository logRepository;
+
+	public void joinV1(String username) {
+		Member member = new Member(username);
+		Log logMessage = new Log(username);
+
+		// 현재 트랜잭션을 각각 사용중
+		log.info("==memberRepository 호출 시작 ==");
+		memberRepository.save(member);
+		log.info("==memberRepository 호출 종료 ==");
+
+		log.info("==logRepository 호출 시작 ==");
+		logRepository.save(logMessage);
+		log.info("==logRepository 호출 종료 ==");
+	}
+
+	public void joinV2(String username) {
+		Member member = new Member(username);
+		Log logMessage = new Log(username);
+
+		// 현재 트랜잭션을 각각 사용중
+		log.info("==memberRepository 호출 시작 ==");
+		memberRepository.save(member);
+		log.info("==memberRepository 호출 종료 ==");
+
+		log.info("==logRepository 호출 시작 ==");
+		try {
+			logRepository.save(logMessage);
+		} catch (RuntimeException e) {
+			log.info("log 저장 실패. logMessage={}", logMessage.getMessage());
+			log.info("정상 흐름 반환");
+		}
+		log.info("==logRepository 호출 종료 ==");
+	}
+
+}
