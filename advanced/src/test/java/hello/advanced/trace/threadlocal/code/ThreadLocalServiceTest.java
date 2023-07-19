350a75edba202e45747a5ce8968b157eaeb12955
ykmxxi <ykmxxi97@gmail.com>
2023-07-19 15:56:51 +0900

    Test #53- apply thread local test
    - ThreadLocal을 적용해 동시성 문제 해결

---
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalServiceTest.java
@@ -0,0 +1,46 @@
+package hello.advanced.trace.threadlocal.code;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class ThreadLocalServiceTest {
+
+	private ThreadLocalService service = new ThreadLocalService();
+
+	@Test
+	void field() {
+		log.info("main start");
+
+		Runnable userA = () -> {
+			service.logic("userA");
+		};
+
+		Runnable userB = () -> {
+			service.logic("userB");
+		};
+
+		Thread threadA = new Thread(userA);
+		threadA.setName("thread-A");
+		Thread threadB = new Thread(userB);
+		threadB.setName("thread-B");
+
+		threadA.start(); // Thread-A 실행
+		// sleep(2000); // 동시성 문제 발생 X
+		sleep(100); // 동시성 문제 발생 X
+		threadB.start(); // Thread-B 실행
+
+		sleep(3000); // 메인 쓰레드 종료 대기
+		log.info("main exit");
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
