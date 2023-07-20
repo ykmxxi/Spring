5d6c21e9d91dceff344d30c76a41f3183cd8a06e
ykmxxi <ykmxxi97@gmail.com>
2023-07-20 14:13:09 +0900

    Test #55- template method by anonymous inner class v2 test
    - 익명 내부 클래스를 이용해 템플릿 메서드 패턴 적용 테스트

--- advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -55,4 +55,29 @@ class TemplateMethodTest {
 		template2.execute();
 	}
 
+	/**
+	 * 템플릿 메서드 패턴 적용, 익명 내부 클래스 적용
+	 */
+	@DisplayName("템플릿 메서드 패턴 적용, 익명 내부 클래스 적용")
+	@Test
+	void templateMethodV2() {
+		AbstractTemplate template1 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직 1 실행");
+			}
+		};
+		log.info("클래스 이름1={}", template1.getClass());
+		template1.execute();
+
+		AbstractTemplate template2 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직 2 실행");
+			}
+		};
+		log.info("클래스 이름2={}", template2.getClass());
+		template2.execute();
+	}
+
 }
