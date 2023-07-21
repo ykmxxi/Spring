7e477ca571d92ff00e8e3d7f9622ee7be05c905c
ykmxxi <ykmxxi97@gmail.com>
2023-07-21 16:15:16 +0900

    Test #55- strategy pattern test
    - 필드에 전략을 보관하는 전략 패턴 테스트

--- advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,7 +1,11 @@
 package hello.advanced.trace.strategy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -35,6 +39,21 @@ class ContextV1Test {
 		long endTime = System.currentTimeMillis();
 		long resultTime = endTime - startTime;
 		log.info("resultTime={}", resultTime);
+	}
+
+	/**
+	 * 전략 패턴 사용
+	 */
+	@DisplayName("전략 패턴 사용: 필드에 전략을 보관")
+	@Test
+	void strategyV1() {
+		StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+		ContextV1 context1 = new ContextV1(strategyLogic1);
+		context1.execute();
+
+		StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+		ContextV1 context2 = new ContextV1(strategyLogic2);
+		context2.execute();
 	}
 
 }
