6ea79d519110f7254331cb79e4bcf96277363264
ykmxxi <ykmxxi97@gmail.com>
2023-07-24 10:23:58 +0900

    Test #57- additional feat by decorator pattern test

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,6 +15,14 @@ public class DecoratorPatternTest {
 	void noDecorator() {
 		Component realComponent = new RealComponent();
 		DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+		client.execute();
+	}
+
+	@Test
+	void decorator1() {
+		Component realComponent = new RealComponent();
+		Component messageDecorator = new MessageDecorator(realComponent);
+		DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
 		client.execute();
 	}
 
