a3c4d3170a85e03527a329e86b46e1a1ed2545eb
ykmxxi <ykmxxi97@gmail.com>
2023-07-24 10:43:23 +0900

    Test #57- proxy chain

--- proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -6,6 +6,7 @@ import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -23,6 +24,15 @@ public class DecoratorPatternTest {
 		Component realComponent = new RealComponent();
 		Component messageDecorator = new MessageDecorator(realComponent);
 		DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+		client.execute();
+	}
+
+	@Test
+	void decorator2() {
+		Component realComponent = new RealComponent();
+		Component messageDecorator = new MessageDecorator(realComponent);
+		Component timeDecorator = new TimeDecorator(messageDecorator);
+		DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
 		client.execute();
 	}
 
