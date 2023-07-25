9d532824e3e7d281276398d248b3cc49fec4ce14
ykmxxi <ykmxxi97@gmail.com>
2023-07-25 14:27:50 +0900

    Test #59- using reflection test
    - 클래스와 메서드의 메타정보를 이용해 호출

--- proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -1,5 +1,7 @@
 package hello.proxy.jdkdynamic;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +24,23 @@ class ReflectionTest {
 		String result2 = target.callB(); // 호출 메서드만 다름
 		log.info("result={}", result2);
 		// 공통 로직2 종료
+	}
+
+	@Test
+	void reflection1() throws Exception {
+		// 클래스 정보
+		Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+		Hello target = new Hello();
+		// callA 메서드 정보
+		Method methodCallA = classHello.getMethod("callA");
+		Object result1 = methodCallA.invoke(target);
+		log.info("result1={}", result1);
+
+		// callB 메서드 정보
+		Method methodCallB = classHello.getMethod("callB");
+		Object result2 = methodCallB.invoke(target);
+		log.info("result2={}", result2);
 	}
 
 	@Slf4j
