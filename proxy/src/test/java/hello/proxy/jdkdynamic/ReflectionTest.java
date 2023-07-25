e8b8ea03422fb5be88bcb76c381f3abcbec87487
ykmxxi <ykmxxi97@gmail.com>
2023-07-25 14:18:50 +0900

    Test #59- not applied reflection basic test
    - 호출 메서드만 다른 공통 로직 구현

---
+++ proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,42 @@
+package hello.proxy.jdkdynamic;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class ReflectionTest {
+
+	@Test
+	void reflection0() {
+		Hello target = new Hello();
+
+		// 공통 로직1 시작
+		log.info("start");
+		String result1 = target.callA(); // 호출 메서드만 다름
+		log.info("result={}", result1);
+		// 공통 로직1 종료
+
+		// 공통 로직2 시작
+		log.info("start");
+		String result2 = target.callB(); // 호출 메서드만 다름
+		log.info("result={}", result2);
+		// 공통 로직2 종료
+	}
+
+	@Slf4j
+	static class Hello {
+
+		public String callA() {
+			log.info("callA");
+			return "A";
+		}
+
+		public String callB() {
+			log.info("callB");
+			return "B";
+		}
+
+	}
+
+}
