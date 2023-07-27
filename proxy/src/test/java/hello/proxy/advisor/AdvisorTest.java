8572d164ce01258a58a8f868f195f5c0f2299d33
ykmxxi <ykmxxi97@gmail.com>
2023-07-27 11:40:52 +0900

    Implement #61- pointcut and method matcher
    - 포인트컷 직접 구현

--- proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -1,6 +1,8 @@
 package hello.proxy.advisor;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -21,6 +23,45 @@ public class AdvisorTest {
 
 		proxy.save();
 		proxy.find();
+	}
+
+
+	static class MyPointcut implements Pointcut {
+
+		@Override
+		public ClassFilter getClassFilter() {
+			return ClassFilter.TRUE;
+		}
+
+		@Override
+		public MethodMatcher getMethodMatcher() {
+			return new MyMethodMatcher();
+		}
+
+	}
+
+	static class MyMethodMatcher implements MethodMatcher {
+
+		private String matchName = "save";
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass) {
+			boolean result = method.getName().equals(matchName);
+			log.info("포인트컷 호출 method={} targetClass={}", method.getName(), targetClass);
+			log.info("포인트컷 결과 result={}", result);
+			return result;
+		}
+
+		@Override
+		public boolean isRuntime() {
+			return false;
+		}
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass, Object... args) {
+			return false;
+		}
+
 	}
 
 }
