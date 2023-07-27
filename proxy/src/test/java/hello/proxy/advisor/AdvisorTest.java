3e684b9a64f6a8ed65e4608b9fd395304e010ce5
ykmxxi <ykmxxi97@gmail.com>
2023-07-27 11:42:08 +0900

    Test #61- directly implemented pointcut test
    - 직접 구현한 포인트컷 테스트
    - save() 어드바이스 적용
    - find() 어드바이스 적용X

--- proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -1,5 +1,8 @@
 package hello.proxy.advisor;
 
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.ClassFilter;
 import org.springframework.aop.MethodMatcher;
@@ -10,7 +13,9 @@ import org.springframework.aop.support.DefaultPointcutAdvisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AdvisorTest {
 
 	@Test
@@ -25,6 +30,18 @@ public class AdvisorTest {
 		proxy.find();
 	}
 
+	@Test
+	@DisplayName("직접 구현한 포인트컷")
+	void advisorTest2() {
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+		proxyFactory.addAdvisor(advisor);
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+
+		proxy.save();
+		proxy.find();
+	}
 
 	static class MyPointcut implements Pointcut {
 
