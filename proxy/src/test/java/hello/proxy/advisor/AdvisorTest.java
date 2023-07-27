0d9aac120c437cb6a67f48558c8e222d074bdf4b
ykmxxi <ykmxxi97@gmail.com>
2023-07-27 11:52:33 +0900

    Test #61- NameMatchMethodPointcut by spring
    - 스프링이 제공하는 포인트컷 테스트
    - 메서드 이름을 기반으로 매칭하는 포인트컷

--- proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -9,6 +9,7 @@ import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
@@ -36,6 +37,24 @@ public class AdvisorTest {
 		ServiceInterface target = new ServiceImpl();
 		ProxyFactory proxyFactory = new ProxyFactory(target);
 		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+		proxyFactory.addAdvisor(advisor);
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+
+		proxy.save();
+		proxy.find();
+	}
+
+	@Test
+	@DisplayName("스프링이 제공하는 포인트컷")
+	void advisorTest3() {
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+
+		// 스프링이 제공하는 포인트컷
+		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+		pointcut.setMappedName("save");
+
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
 		proxyFactory.addAdvisor(advisor);
 		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
 
