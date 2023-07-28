4834c3498f60436028a41a0953b2869da3e28c11
ykmxxi <ykmxxi97@gmail.com>
2023-07-28 10:33:19 +0900

    Test #61- multi advisor and one proxy test
    - 여러개의 어드바이저를 하나의 프록시에 적용
    - 성능 향상

--- proxy/src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
+++ proxy/src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -36,6 +36,25 @@ class MultiAdvisorTest {
 		proxy2.save();
 	}
 
+	@Test
+	@DisplayName("하나의 프록시, 여러 어드바이저")
+	void multiAdvisorTest2() {
+		// client -> proxy -> advisor2 -> advisor1 -> target
+
+		DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+		DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+		// 프록시 생성
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+
+		proxyFactory.addAdvisor(advisor2);
+		proxyFactory.addAdvisor(advisor1);
+
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+		proxy.save();
+	}
+
 	static class Advice1 implements MethodInterceptor {
 
 		@Override
