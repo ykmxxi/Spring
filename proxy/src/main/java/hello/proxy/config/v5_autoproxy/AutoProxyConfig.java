1d01600f946a820cae37f76aa17882061a868a0f
ykmxxi <ykmxxi97@gmail.com>
2023-08-01 13:50:37 +0900

    Implement #63- pointcut by aspectJ expression
    - AspectJ 표현식을 이용한 세밀한 포인트컷 지정

--- proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -1,6 +1,7 @@
 package hello.proxy.config.v5_autoproxy;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -16,11 +17,23 @@ import hello.proxy.trace.logtrace.LogTrace;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-	@Bean
+	// @Bean
 	public Advisor advisor1(LogTrace logTrace) {
 		// pointcut
 		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
 		pointcut.setMappedNames("request*", "order*", "save*");
+
+		// advice
+		LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+
+	@Bean
+	public Advisor advisor2(LogTrace logTrace) {
+		// pointcut
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..))");
 
 		// advice
 		LogTraceAdvice advice = new LogTraceAdvice(logTrace);
