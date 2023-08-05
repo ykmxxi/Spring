53e693a9510435da77deb98a271d3246ab21a74c
ykmxxi <ykmxxi97@gmail.com>
2023-08-05 14:09:45 +0900

    Implement #67- aspect v5
    - 어드바이스 실행 순서 지정
    - `@Order` 사용

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -10,14 +10,15 @@ import org.springframework.context.annotation.Import;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 // @Import(AspectV1.class)
 // @Import(AspectV2.class)
 // @Import(AspectV3.class)
-@Import(AspectV4Pointcut.class)
+// @Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 @SpringBootTest
 public class AopTest {
 
