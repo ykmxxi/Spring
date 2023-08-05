a3527d7e02d7dc0ef5e2f4680979f72c546781c4
ykmxxi <ykmxxi97@gmail.com>
2023-08-05 11:55:14 +0900

    Add #67- register aspect as spring bean
    - Aspect를 스프링 빈으로 등록

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Import(AspectV1.class)
 @SpringBootTest
 public class AopTest {
 
