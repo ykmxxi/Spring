e286f017b2e7fbcff145e5ec57857ebb938a91b8
ykmxxi <ykmxxi97@gmail.com>
2023-08-08 10:41:30 +0900

    Test #69- parameter pointcut matching

--- aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ aop/src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -131,4 +131,44 @@ public class ExecutionTest {
 		assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isFalse();
 	}
 
+	// String 타입의 파라미터 허용
+	// (String)
+	@Test
+	void argsMatch() {
+		pointcut.setExpression("execution(* *(String))");
+		assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+	}
+
+	// 파라미터가 없어야 함
+	// ()
+	@Test
+	void argsMatchNoArgs() {
+		pointcut.setExpression("execution(* *())");
+		assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+	}
+
+	// 정확히 하나의 파라미터 허용, 모든 타입 허용
+	// (Xxx)
+	@Test
+	void argsMatchStar() {
+		pointcut.setExpression("execution(* *(*))");
+		assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+	}
+
+	// 숫자와 무관하게 모든 파라미터, 모든 타입 허용
+	// (), (Xxx), (Xxx, Xxx)
+	@Test
+	void argsMatchAll() {
+		pointcut.setExpression("execution(* *(..))");
+		assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+	}
+
+	// String 타입으로 시작, 숫자와 무관하게 모든 파라미터, 모든 타입 허용
+	// (String), (String, Xxx), (String, Xxx, Xxx) 허용
+	@Test
+	void argsMatchComplex() {
+		pointcut.setExpression("execution(* *(String, ..))");
+		assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+	}
+
 }
