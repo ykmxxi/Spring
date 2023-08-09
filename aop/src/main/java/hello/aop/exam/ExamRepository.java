6837cb0359d251389141461d8113b630ef449dca
ykmxxi <ykmxxi97@gmail.com>
2023-08-09 09:32:38 +0900

    Add #71- applying log trace aspect
    - 서비스와 리포지토리 메서드에 애노테이션 추가
    - 테스트 코드 Aspect 스프링 빈 추가

--- aop/src/main/java/hello/aop/exam/ExamRepository.java
+++ aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -2,6 +2,8 @@ package hello.aop.exam;
 
 import org.springframework.stereotype.Repository;
 
+import hello.aop.exam.annotation.Trace;
+
 @Repository
 public class ExamRepository {
 
@@ -10,6 +12,7 @@ public class ExamRepository {
 	/**
 	 * 5번 실행 중 1번 실패하는 요청
 	 */
+	@Trace
 	public String save(String itemId) {
 		seq++;
 		if (seq % 5 == 0) {
