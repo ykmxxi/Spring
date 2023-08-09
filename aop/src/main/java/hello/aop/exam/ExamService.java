c9e0a27dd0779108d552cb6f75976ed66a055e32
ykmxxi <ykmxxi97@gmail.com>
2023-08-09 09:06:51 +0900

    Implement #71- repository and service for example
    - 5번 요청 중 1번 실패하는 저장소

---
+++ aop/src/main/java/hello/aop/exam/ExamService.java
@@ -0,0 +1,17 @@
+package hello.aop.exam;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+	private final ExamRepository examRepository;
+
+	public void request(String itemId) {
+		examRepository.save(itemId);
+	}
+
+}
